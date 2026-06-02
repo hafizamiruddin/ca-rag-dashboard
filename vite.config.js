608f4143b0708b10bf47b29ddeaeeb82c59e7743
Hafiz Amiruddin <hafizishikawa@gmail.com>
2026-06-02 12:25:26 +0800

    configure for github pages deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // For GitHub Pages: change base to '/your-repo-name/'
-  base: '/',
+  base: '/ca-rag-dashboard/',
 })
